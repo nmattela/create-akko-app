356d6913e7d234e4125ff1f08f260f635bfa6038
nmattela <nicolas.mattelaer@gmail.com>
2020-03-15 12:57:05 +0100

    Fixed it

--- index.js
+++ index.js
@@ -2,31 +2,15 @@
 const { execSync } = require('child_process');
 const fs = require('fs');
 
-
 const appName = process.argv[2];
 const path = `${process.cwd()}/${appName}`;
 
-execSync(`mkdir ${appName}`);
-execSync(`npm init -y`, {cwd: path});
-execSync(`npm install --save-dev @babel/cli @babel/core @babel/plugin-proposal-class-properties @babel/preset-env babel-loader babel-plugin-include babel-plugin-transform-jsx webpack webpack-cli webpack-dev-server css-loader html-loader html-webpack-plugin style-loader akko-js`, {cwd: path});
-
+console.log(`Fetching latest template...`);
+execSync(`git clone https://github.com/nmattela/akko-js-framework.git ${appName}`);
+console.log(`Modifying package.json...`);
 const pkg = JSON.parse(fs.readFileSync(`${path}/package.json`));
-pkg.scripts = {
-    "build": "webpack",
-    "test": "webpack-dev-server"
-};
+pkg.name = appName;
 fs.writeFileSync(`${path}/package.json`, JSON.stringify(pkg, undefined, 2));
-
-function writeFiles(rootPath, extension) {
-    const contents = fs.readdirSync(`${rootPath}${extension}`);
-    contents.forEach(content => {
-        if(fs.lstatSync(`${rootPath}${extension}${content}`).isDirectory()) {
-            fs.mkdirSync(`${path}/${extension}${content}`);
-            writeFiles(rootPath, `${extension}${content}/`);
-        } else {
-            fs.writeFileSync(`${path}/${extension}${content}`, fs.readFileSync(`${rootPath}${extension}${content}`, 'utf8'))
-        }
-    })
-}
-
-writeFiles(`./files/`, '');
+console.log(`Installing modules...`);
+execSync(`npm install`, {cwd: path});
+console.log(`Done! Your app is available in ${path}/${appName}. Use npm run test to start up a development server.`);
